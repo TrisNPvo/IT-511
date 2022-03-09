2657b7789bf70c111106722a25b9c249af697436
TrisNPvo <101231233+TrisNPvo@users.noreply.github.com>
2022-03-08 22:04:11 -0500

    Add files via upload

---
+++ RecipeBox/dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"recipebox","l":"Ingredient"},{"p":"recipebox","l":"Recipe"},{"p":"recipebox","l":"RecipeBox"}];updateSearchResults();
